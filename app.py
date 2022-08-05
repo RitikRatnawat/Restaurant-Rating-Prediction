79012f30a1678ca1d85d7e25ea367badb30ecbb8
Ritik Ratnawat <ritikratnawat786@gmail.com>
2022-08-05 12:49:40 +0530

    Configuring Development Server

---
+++ app.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from flask import Flask, render_template, request
+import pandas as pd
+import pickle
+import sklearn
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+@app.route("/predictor", methods = ["GET", "POST"])
+def predictor():
+    return render_template("predictor.html")
+
+@app.route("/aboutUs")
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
