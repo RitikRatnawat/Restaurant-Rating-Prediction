86d5076f5778cb7e64fa4d8bae7989b3287f6b4e
Ritik Ratnawat <ritikratnawat786@gmail.com>
2022-08-13 19:18:08 +0530

    Adding Locations

--- app.py
+++ app.py
@@ -12,6 +12,45 @@ def welcome():
 
 @app.route("/predictor", methods = ["GET", "POST"])
 def predictor():
+    columns = ['online_order', 'book_table', 'votes', 'approx_cost', 'Bakery',
+                'Food Truck', 'Pub', 'Club', 'Bhojanalya', 'Delivery', 'Quick Bites', 
+                'Irani Cafee', 'Takeaway', 'Dessert Parlor', 'Food Court', 'Sweet Shop', 
+                'Cafe', 'Casual Dining', 'Beverage Shop', 'Microbrewery', 'Kiosk', 'Fine Dining', 
+                'Lounge', 'Meat Shop', 'Bar', 'Pop Up', 'Confectionery', 'Mess', 'Dhaba', 
+                'Australian', 'Tibetan', 'Cantonese', 'Mithai', 'Desserts', 'British',
+                'Tea', 'Grill', 'Hot dogs', 'Gujarati', 'American', 'Afghani',
+                'Mangalorean', 'Mughlai', 'Chinese', 'Mexican', 'Raw Meats',
+                'Mongolian', 'Kerala', 'Pizza', 'Italian', 'Seafood', 'Sri Lankan',
+                'Bihari', 'Vegan', 'Greek', 'Kebab', 'Lebanese', 'Lucknowi', 'Malwani',
+                'Arabian', 'Pan Asian', 'Iranian', 'Nepalese', 'Jewish', 'Sindhi',
+                'North Indian', 'Steak', 'Beverages', 'Indian', 'Spanish', 'Afghan',
+                'Salad', 'Russian', 'Bar Food', 'African', 'Middle Eastern',
+                'Maharashtrian', 'Ice Cream', 'Wraps', 'Thai', 'Konkan', 'Bubble Tea',
+                'Fast Food', 'Assamese', 'Oriya', 'Finger Food', 'Parsi', 'French',
+                'Roast Chicken', 'Korean', 'Japanese', 'Tex-Mex', 'Naga', 'Rajasthani',
+                'Biryani', 'Charcoal Chicken', 'Sushi', 'Juices', 'Singaporean', 'Drinks Only', 
+                'Momos', 'Vietnamese', 'BBQ', 'Coffee', 'Kashmiri','European', 'Modern Indian', 
+                'Mediterranean', 'Healthy Food', 'Andhra', 'North Eastern', 'German', 'Chettinad', 
+                'Continental', 'Bengali', 'Hyderabadi', 'Indonesian', 'Malaysian', 'Paan', 'Burger', 
+                'Sandwich', 'Bohri', 'South Indian', 'Goan', 'Tamil', 'Portuguese', 'South American', 
+                'Awadhi', 'Belgian', 'Street Food', 'Turkish', 'Rolls', 'Asian', 'Burmese', 'BTM', 
+                'Banashankari', 'Banaswadi', 'Bannerghatta Road', 'Basavanagudi', 'Basaveshwara Nagar', 
+                'Bellandur', 'Bommanahalli', 'Brigade Road', 'Brookefield', 'CV Raman Nagar',
+                'Central Bangalore', 'Church Street', 'City Market', 'Commercial Street', 'Cunningham Road', 
+                'Domlur', 'East Bangalore', 'Ejipura', 'Electronic City', 'Frazer Town', 'HBR Layout', 'HSR', 
+                'Hebbal', 'Hennur', 'Hosur Road', 'ITPL Main Road, Whitefield', 'Indiranagar','Infantry Road', 
+                'JP Nagar', 'Jakkur', 'Jalahalli', 'Jayanagar', 'Jeevan Bhima Nagar', 'KR Puram', 'Kaggadasapura', 
+                'Kalyan Nagar', 'Kammanahalli', 'Kanakapura Road', 'Kengeri', 'Koramangala','Koramangala 1st Block',
+                'Koramangala 2nd Block','Koramangala 3rd Block', 'Koramangala 4th Block', 'Koramangala 5th Block', 
+                'Koramangala 6th Block', 'Koramangala 7th Block', 'Koramangala 8th Block', 'Kumaraswamy Layout',
+                'Langford Town', 'Lavelle Road', 'MG Road', 'Magadi Road', 'Majestic', 'Malleshwaram', 'Marathahalli', 
+                'Mysore Road', 'Nagarbhavi', 'Nagawara', 'New BEL Road', 'North Bangalore', 'Old Airport Road',
+                'Old Madras Road', 'Peenya', 'RT Nagar', 'Race Course Road', 'Rajajinagar', 'Rajarajeshwari Nagar',
+                'Rammurthy Nagar', 'Residency Road', 'Richmond Road', 'Sadashiv Nagar', 'Sahakara Nagar', 'Sanjay Nagar', 
+                'Sankey Road', 'Sarjapur Road', 'Seshadripuram', 'Shanti Nagar', 'Shivajinagar', 'South Bangalore',
+                'St. Marks Road', 'Thippasandra', 'Ulsoor', 'Uttarahalli', 'Varthur Main Road, Whitefield', 'Vasanth Nagar', 
+                'Vijay Nagar', 'West Bangalore', 'Whitefield', 'Wilson Garden', 'Yelahanka', 'Yeshwantpur']
+     
     return render_template("predictor.html")
 
 @app.route("/aboutUs")
--- static/script.js
+++ static/script.js
@@ -1,7 +1,7 @@
 function addOptions()
 {
     // Adding Restaurant Types
-    const restTypes = ['Select-Rest-Type', 'Lounge', 'Kiosk', 'Food Truck', 'Irani Cafee',
+    const restTypes = [ 'Lounge', 'Kiosk', 'Food Truck', 'Irani Cafee',
                         'Confectionery', 'Cafe', 'Microbrewery', 'Dhaba', 'Takeaway', 'Pop Up',
                         'Pub', 'Bar', 'Delivery', 'Meat Shop', 'Beverage Shop', 'Dessert Parlor',
                         'Club', 'Quick Bites', 'Bhojanalya', 'Casual Dining', 'Fine Dining', 'Mess',
@@ -16,7 +16,7 @@ function addOptions()
 
 
     // Adding Cuisines
-    const cuisines = ["Select-Cuisines", "Rajasthani", "Mediterranean", "Desserts", "Burmese",
+    const cuisines = [ "Rajasthani", "Mediterranean", "Desserts", "Burmese",
                         "Tibetan", "Belgian", "Seafood", "Raw Meats", "Greek", "African", "Russian",
                         "Italian", "Grill", "Korean", "Ice Cream", "Coffee", "Spanish", "Nepalese",
                         "Afghani", "Momos", "Bar Food", "South American", "Middle Eastern", "Malaysian",
@@ -38,6 +38,41 @@ function addOptions()
     for(let i = 0; i < cuisines.length; i++)
     {
         dropdown.options[dropdown.options.length] = new Option(cuisines[i], cuisines[i]);
+    }
+
+
+    // Adding Locations
+    const locations = ['BTM', 'Banashankari', 'Banaswadi', 'Bannerghatta Road', 'Basavanagudi',
+                        'Basaveshwara Nagar', 'Bellandur', 'Bommanahalli', 'Brigade Road',
+                        'Brookefield', 'CV Raman Nagar', 'Central Bangalore', 'Church Street',
+                        'City Market', 'Commercial Street', 'Cunningham Road', 'Domlur',
+                        'East Bangalore', 'Ejipura', 'Electronic City', 'Frazer Town',
+                        'HBR Layout', 'HSR', 'Hebbal', 'Hennur', 'Hosur Road',
+                        'ITPL Main Road, Whitefield', 'Indiranagar', 'Infantry Road',
+                        'JP Nagar', 'Jakkur', 'Jalahalli', 'Jayanagar', 'Jeevan Bhima Nagar',
+                        'KR Puram', 'Kaggadasapura', 'Kalyan Nagar', 'Kammanahalli',
+                        'Kanakapura Road', 'Kengeri', 'Koramangala', 'Koramangala 1st Block',
+                        'Koramangala 2nd Block', 'Koramangala 3rd Block',
+                        'Koramangala 4th Block', 'Koramangala 5th Block',
+                        'Koramangala 6th Block', 'Koramangala 7th Block',
+                        'Koramangala 8th Block', 'Kumaraswamy Layout', 'Langford Town',
+                        'Lavelle Road', 'MG Road', 'Magadi Road', 'Majestic', 'Malleshwaram',
+                        'Marathahalli', 'Mysore Road', 'Nagarbhavi', 'Nagawara', 'New BEL Road',
+                        'North Bangalore', 'Old Airport Road', 'Old Madras Road', 'Peenya',
+                        'RT Nagar', 'Race Course Road', 'Rajajinagar', 'Rajarajeshwari Nagar',
+                        'Rammurthy Nagar', 'Residency Road', 'Richmond Road', 'Sadashiv Nagar',
+                        'Sahakara Nagar', 'Sanjay Nagar', 'Sankey Road', 'Sarjapur Road',
+                        'Seshadripuram', 'Shanti Nagar', 'Shivajinagar', 'South Bangalore',
+                        'St. Marks Road', 'Thippasandra', 'Ulsoor', 'Uttarahalli',
+                        'Varthur Main Road, Whitefield', 'Vasanth Nagar', 'Vijay Nagar',
+                        'West Bangalore', 'Whitefield', 'Wilson Garden', 'Yelahanka',
+                        'Yeshwantpur'];
+
+    dropdown = document.getElementById("L");
+
+    for(let i = 0; i < locations.length; i++)
+    {
+        dropdown.options[dropdown.options.length] = new Option(locations[i], locations[i]);
     }
 }
 
